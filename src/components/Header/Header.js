809449dc9d6bbde6d187c72cea72aab887522166
Pliavi <pliavi.rpgm@gmail.com>
2019-02-11 21:22:08 -0200

    move header to a separate file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+const html = `
+  <header class="header">
+    <div class="logo">LOGO</div>
+    <div class="search">
+      <div class="-wrapper">
+        <input type="search" class="-bar">
+      </div>
+    </div>
+    <nav class="nav">
+      <a href="#" class="nav-item">Produtos</a>
+      <a href="#" class="nav-item">Serviços</a>
+      <a href="#" class="nav-item">Carrinho<span class="badge" id="cart-counter">10</span></a>
+    </nav>
+  </header>
+`;

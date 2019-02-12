fb51aff66aa8d8310906d1844c125a10ab5794fc
Pliavi <pliavi.rpgm@gmail.com>
2019-02-12 00:01:36 -0200

    simplify index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,20 @@
-import { createCard } from "./components/Card";
-import { createSpecs } from "./components/Description";
-
-const $cards = document.querySelector(".cards");
-$cards.innerHTML = "";
+import { createCard } from "./components/Card/Card";
+import { createHeader } from "./components/Header/Header";
+import "./css/main.css";
 
 fetch("./mocks/Products.json")
   .then(data => data.json())
-  .then(products => {
-    const randomProduct = Math.trunc(Math.random() * 3);
-    const product = products[randomProduct];
-    const $card = createCard(product);
-    const $specs = createSpecs(product.specs);
+  .then(createProductCard);
 
-    $cards.insertAdjacentElement("beforeend", $card);
-    $cards.insertAdjacentElement("beforeend", $specs);
+function createProductCard(products) {
+  const randomProduct = Math.trunc(Math.random() * 3);
+  const product = products[randomProduct];
 
-    setTimeout(() => $specs.classList.add("--start"), 10);
-    init();
-  });
+  const $root = document.getElementById("root");
+  const $header = createHeader();
+  const $card = createCard(product);
 
-function init() {
-  const $searchBar = document.querySelector(".search .-bar");
-  const $search = document.querySelector(".search");
-  $searchBar.addEventListener("focus", () => $search.classList.add("--active"));
-  $searchBar.addEventListener("blur", () =>
-    $search.classList.remove("--active")
-  );
-
-  let cartCounter = 10;
-  const $buyButton = document.querySelector(".buy-button");
-  const $cartCounter = document.getElementById("cart-counter");
-  $buyButton.addEventListener("click", () => {
-    $cartCounter.innerText = ++cartCounter;
-  });
+  $root.innerHTML = "";
+  $root.insertAdjacentElement("afterbegin", $header);
+  $root.insertAdjacentElement("beforeend", $card);
 }

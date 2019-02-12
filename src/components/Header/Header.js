569198f5ba9f01d0e862f3a1e949123626746656
Pliavi <pliavi.rpgm@gmail.com>
2019-02-11 23:59:52 -0200

    Refatore header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,37 @@
-const html = `
-  <header class="header">
+import { cartCounter } from "./../../store";
+import "./Header.css";
+
+export function createHeader() {
+  const $header = document.createElement("header");
+
+  $header.classList.add("header");
+  $header.innerHTML = `
     <div class="logo">LOGO</div>
     <div class="search">
-      <div class="-wrapper">
-        <input type="search" class="-bar">
+      <div class="-bar">
+        <input type="search" class="-input">
       </div>
     </div>
     <nav class="nav">
-      <a href="#" class="nav-item">Produtos</a>
-      <a href="#" class="nav-item">Serviços</a>
-      <a href="#" class="nav-item">Carrinho<span class="badge" id="cart-counter">10</span></a>
+      <a href="#" class="-item">Produtos</a>
+      <a href="#" class="-item">Serviços</a>
+      <a href="#" class="-item">Carrinho<span class="badge" id="cart-counter">0</span></a>
     </nav>
-  </header>
-`;
+  `;
+
+  cartCounter.subscribe($header.querySelector("#cart-counter"));
+
+  const $search = $header.querySelector(".search");
+  const $searchInput = $search.querySelector(".-input");
+
+  $searchInput.addEventListener("focus", () => {
+    $search.classList.add("--active");
+    $searchInput.select();
+  });
+
+  $searchInput.addEventListener("blur", () =>
+    $search.classList.remove("--active")
+  );
+
+  return $header;
+}
